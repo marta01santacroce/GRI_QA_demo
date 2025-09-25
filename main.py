fce11e500700cb250f62d482139cbebb80524362
Marta Santacroce <82643850+marta01santacroce@users.noreply.github.com>
2025-09-25 12:25:35 +0200

    Update main.py

--- main.py
+++ main.py
@@ -8,100 +8,162 @@ import json
 from itertools import islice
 from bs4 import BeautifulSoup
 
-
 if __name__ == "__main__":
-    
-    
+
     args = init_args()
     r = Runnable(args)
 
     if len(args["load_query_from_file"]) > 0:
-      
-      with open(args["load_query_from_file"], 'r') as file:
-        data = json.load(file)
-      
-      if os.path.isdir(args["pdf"]):
-        file_names = os.listdir(args["pdf"])
-      elif os.path.isfile(args["pdf"]):
-        file_names = [args["pdf"]]
-      else:
-        raise ValueError(f"wrong file name")
-  
-      for file_name in file_names:
-        splitted_file_name = file_name.split(".")
-        if splitted_file_name[-1] != "pdf":
-          continue
-        
-        file_path = args["pdf"]  
-        base_name = os.path.basename(file_path)  
-        dir_name = os.path.splitext(base_name)[0]  
 
+        with open(args["load_query_from_file"], 'r') as file:
+            data = json.load(file)
 
-        args["pdf"] = file_name
-        gri_code_to_page = {}
-        tables_as_html = set()
+        if os.path.isdir(args["pdf"]):
+            file_names = os.listdir(args["pdf"])
+        elif os.path.isfile(args["pdf"]):
+            file_names = [args["pdf"]]
+        else:
+            raise ValueError(f"wrong file name")
 
-        for gri_code, description in islice(data.items(),3,8): #dal 4 all'8 GRI
-          
-          if gri_code not in gri_code_to_page.keys():
-            gri_code_to_page[gri_code] = []
+        for file_name in file_names:
+            splitted_file_name = file_name.split(".")
+            if splitted_file_name[-1] != "pdf":
+                continue
 
-          args["query"] = description
-          r.set_args(args)
-          s = r.run()
+            file_path = args["pdf"]
+            base_name = os.path.basename(file_path)
+            dir_name = os.path.splitext(base_name)[0]
 
-          ute = UnstructuredTableExtractor("yolox", "hi_res")
+            args["pdf"] = file_name
+            gri_code_to_page = {}
+            tables_as_html = set()
 
-          for doc in tqdm(s[:args["k"]]): #keeps only the top k pages with the highest score, where k is specified in the Python command (default = 5) 
+            for gri_code, description in islice(data.items(), 3, 8):  # dal 4 all'8 GRI
 
-            #print("\n\nDEB doc: "+ str(doc))
+                if gri_code not in gri_code_to_page.keys():
+                    gri_code_to_page[gri_code] = []
 
-            tables = ute.extract_table_unstructured([doc]) #extract tables
+                args["query"] = description
+                r.set_args(args)
+                s = r.run()
 
-            #print("\n\nDEB tables: "+ str(tables))
+                ute = UnstructuredTableExtractor("yolox", "hi_res")
 
-            for i,table in enumerate(tables):
-              #print("\n\nDEB table: "+ str(table))
+                for doc in tqdm(s[:args["k"]]):  # keeps only the top k pages with the highest score, where k is specified in the Python command (default = 5)
 
-              #for i in range(len(table)):
-              #print("\n\nDEB table[0]: "+ str(table[0]))
-              
-              tables_as_html.add((table[0].metadata.text_as_html, doc.page_content, doc.metadata["page"], i))
-              gri_code_to_page[gri_code].append((doc.metadata["page"], i))
+                    tables = ute.extract_table_unstructured([doc])  # extract tables
 
-          #print("DEB: " + str(tables_as_html))  
+                    for i, table in enumerate(tables):
+                        tables_as_html.add((table[0].metadata.text_as_html, doc.page_content, doc.metadata["page"], i))
+                        gri_code_to_page[gri_code].append((doc.metadata["page"], i))
 
+            for table_html in tables_as_html:
 
-        for table_html in tables_as_html:
-          #print("DEBUG table_html: " + str(table_html))
+                # table_html[0] contiene il testo della tabella estratta (HTML)
+                raw_table_text = table_html[0]
 
-          # table_html[0] contiene il testo della tabella estratta (HTML)
-          raw_table_text = table_html[0]
+                soup = BeautifulSoup(raw_table_text, "html.parser")
 
-          #print("[DEBUG-raw_table_text]: " + str(raw_table_text))
+                # Trova tutte le righe della tabella
+                rows = []
+                for tr in soup.find_all("tr"):
+                    # Ogni riga può avere sia <td> che <th>
+                    cells = [cell.get_text(strip=True) for cell in tr.find_all(["td", "th"])]
+                    rows.append(cells)
 
-          soup = BeautifulSoup(raw_table_text, "html.parser")
+                # Assicurati che la cartella esista
+                if not os.path.exists(f"table_dataset/{dir_name}"):
+                    os.makedirs(f"table_dataset/{dir_name}", exist_ok=True)
 
-          # Trova tutte le righe della tabella
-          rows = []
-          for tr in soup.find_all("tr"):
-              # Ogni riga può avere sia <td> che <th>
-              cells = [cell.get_text(strip=True) for cell in tr.find_all(["td", "th"])]
-              rows.append(cells)
+                # Salva in CSV
+                with open(f'table_dataset/{dir_name}/{str(table_html[-2])}_{str(table_html[-1])}.csv',
+                          mode='w', newline='', encoding='utf-8') as file:
+                    writer = csv.writer(file)
+                    writer.writerows(rows)
 
-          # Assicurati che la cartella esista
-          if not os.path.exists(f"table_dataset/{dir_name}"):
-              os.makedirs(f"table_dataset/{dir_name}", exist_ok=True)
+            with open(f'table_dataset/{dir_name}/metadata.json', 'w') as json_file:
+                json.dump(gri_code_to_page, json_file, indent=4)
 
-          # Salva in CSV
-          with open(f'table_dataset/{dir_name}/{str(table_html[-2])}_{str(table_html[-1])}.csv',
-                    mode='w', newline='', encoding='utf-8') as file:
-              writer = csv.writer(file)
-              writer.writerows(rows)
+    elif len(args["query"]) > 0:
 
+        if os.path.isdir(args["pdf"]):
+            file_names = os.listdir(args["pdf"])
+        elif os.path.isfile(args["pdf"]):
+            file_names = [args["pdf"]]
+        else:
+            raise ValueError(f"wrong file name")
+        for file_name in file_names:
+            splitted_file_name = file_name.split(".")
+            if splitted_file_name[-1] != "pdf":
+                continue
 
-        with open(f'table_dataset/{dir_name}/metadata.json', 'w') as json_file:
-          json.dump(gri_code_to_page, json_file, indent=4)
-          
+            file_path = args["pdf"]
+            base_name = os.path.basename(file_path)
+            dir_name = os.path.splitext(base_name)[0]
+
+            args["pdf"] = file_name
+            question_to_page = {}
+            tables_as_html = set()
+
+            if args["query"] not in question_to_page.keys():
+                question_to_page[args["query"]] = []
+
+            s = r.run()
+
+            ute = UnstructuredTableExtractor("yolox", "hi_res")
+
+            for doc in tqdm(s[:args["k"]]):  # keeps only the top k pages with the highest score, where k is specified in the Python command (default = 5)
+
+                tables = ute.extract_table_unstructured([doc])  # extract tables
+
+                for i, table in enumerate(tables):
+                    tables_as_html.add((table[0].metadata.text_as_html, doc.page_content, doc.metadata["page"], i))
+                    question_to_page[args["query"]].append((doc.metadata["page"], i))
+
+            # print("\nDEBUG question_to_page: ", question_to_page)
+
+            for table_html in tables_as_html:
+
+                # table_html[0] contiene il testo della tabella estratta (HTML)
+                raw_table_text = table_html[0]
+
+                soup = BeautifulSoup(raw_table_text, "html.parser")
+
+                # Trova tutte le righe della tabella
+                rows = []
+                for tr in soup.find_all("tr"):
+                    # Ogni riga può avere sia <td> che <th>
+                    cells = [cell.get_text(strip=True) for cell in tr.find_all(["td", "th"])]
+                    rows.append(cells)
+
+                # Assicurati che la cartella esista
+                if not os.path.exists(f"table_dataset/{dir_name}/verbal_questions"):
+                    os.makedirs(f"table_dataset/{dir_name}/verbal_questions", exist_ok=True)
+
+                # Salva in CSV
+                with open(f'table_dataset/{dir_name}/verbal_questions/{str(table_html[-2])}_{str(table_html[-1])}.csv',
+                          mode='w', newline='', encoding='utf-8') as file:
+                    writer = csv.writer(file)
+                    writer.writerows(rows)
+
+            metadata_path = f'table_dataset/{dir_name}/verbal_questions/metadata.json'
+
+            if os.path.exists(metadata_path):
+                with open(metadata_path, 'r') as json_file:
+                    try:
+                        existing_data = json.load(json_file)
+                    except json.JSONDecodeError:
+                        existing_data = {}  # file vuoto o corrotto → inizializza dict vuoto
+
+            else:
+                existing_data = {}
+
+            # 2. Aggiorna con i nuovi dati
+            existing_data.update(question_to_page)  # se vuoi unione di dict
+
+            # 3. Riscrivi tutto
+            with open(metadata_path, "w") as json_file:
+                json.dump(existing_data, json_file, indent=4)
+
     else:
-      s = r.run()
+        s = r.run()
