a70199f2f0002782696a89ccdda2caa17597ac91
lucacontalbo <luca.contalbo@gmail.com>
2025-10-08 16:04:56 +0200

    changed prompt normalization

--- prompts/query_agent_prompts.py
+++ prompts/query_agent_prompts.py
@@ -1,11 +1,11 @@
 prompt_extract = """You will be given a question and a table.
-You must indicate the indices of the rows and indices of the columns that could be relevant to answer the question. You must not try to answer the question, you must only retrieve the relevant rows. You must use the values in the "index" column to refer to the relevant rows. For the column indices, write the number (from left to right starting from 0), not the column name.
+You must indicate the indices of the rows and indices of the columns that could be relevant to answer the question. You must not try to answer the question, you must only retrieve the relevant rows. You must use the values in the "index" column to refer to the relevant rows. For the column indices, use the numerical column names.
 First reason step-by-step. Then, write "Final answer: " followed exclusively by a Python dictionary:
 {{
     "rows": [row_index1,...,row_indexn],
     "columns": [column_index1,...,column_indexn],
 }}
-The column indices must start from 0.
+
 Ensure that the final answer is in the expected form. Do not write anything else after "Final answer:". Do not use Markdown syntax.
 
 Question: {question}
@@ -16,7 +16,10 @@ Let's think step-by-step. """
 prompt_normalization = """Given multiple tables and a question, decide the unit of measure to use for the final answer.
 Then, align the table values by converting the needed values to a unique unit of measurement.
 If the question specifies the unit of measurement, convert the values to that unit of measurement. Otherwise, you must decide the unit of measurement and convert the values.
-You must not rewrite the tables. You must only eventually write a list of rules/formulas indicating how to perform the needed transformations.
+You must not rewrite the tables. You must only eventually write a list of rules/formulas indicating how to perform the needed transformations. The transformations must exclusively be about the handling of units of measurement. Do not talk about anything else, including how to solve the question.
+A sample rule can be:
+1. 1000 meters = 1 kilometer
+
 First reason step-by-step. Then write "Final answer: " followed exclusively by the list of rules/formulas.
 
 Do not try to answer the question, but focus only on the given task. Ensure that the final answer is in the expected form. Do not write anything else after "Final answer:". Do not use Markdown syntax.
